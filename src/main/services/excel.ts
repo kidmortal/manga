b7e61f7f452ed3a06ea7619ddf0615f89162359e
kidmortal <kidmortal@gmail.com>
2022-02-21 11:38:56 -0300

    fix path 4

--- src/main/services/excel.ts
+++ src/main/services/excel.ts
@@ -6,19 +6,22 @@ export async function ExcelWriteCarimbo(carimbo: Carimbo) {
   const workbook = new ExcelJS.Workbook();
   try {
     const file = await workbook.xlsx.readFile(
-      join(app.getPath('userData'), '/CARIMBO.xlsx')
+      join(app.getPath('userData'), '\\CARIMBO.xlsx')
     );
     const worksheet = file.getWorksheet('CARIMBO');
     const name = worksheet.getCell(8, 2);
     const info = worksheet.getCell(8, 3);
     name.value = carimbo.NOME;
     info.value = carimbo['CNPJ/CPF'];
-    workbook.xlsx.writeFile(join(app.getPath('userData'), '/CARIMBO.xlsx'));
+    workbook.xlsx.writeFile(join(app.getPath('userData'), '\\CARIMBO.xlsx'));
     return true;
   } catch (error) {
     dialog.showMessageBox({
       title: 'Error',
-      message: `Arquivo CARIMBO.xlsx não localizado, \n verifique em ${process.cwd()} `,
+      message: `Arquivo ${join(
+        app.getPath('userData'),
+        '\\CARIMBO.xlsx'
+      )} não localizado, \n verifique em ${app.getPath('userData')} `,
     });
     return false;
   }
@@ -28,7 +31,7 @@ export async function ExcelWriteEnvelope(envelope: Envelope) {
   const workbook = new ExcelJS.Workbook();
   try {
     const file = await workbook.xlsx.readFile(
-      join(app.getPath('userData'), '/ENVELOPE.xlsx')
+      join(app.getPath('userData'), '\\ENVELOPE.xlsx')
     );
     const worksheet = file.getWorksheet('ENVELOPE');
     const name = worksheet.getCell(1, 1);
@@ -48,14 +51,15 @@ export async function ExcelWriteEnvelope(envelope: Envelope) {
     especieValue.value = valorEspecie > 0 ? valorEspecie : '';
     chequesLabel.value = valorCheques > 0 ? 'CHEQUES' : '';
     especieLabel.value = valorEspecie > 0 ? 'ESPECIE' : '';
-    workbook.xlsx.writeFile(join(app.getPath('userData'), '/ENVELOPE.xlsx'));
+    workbook.xlsx.writeFile(join(app.getPath('userData'), '\\ENVELOPE.xlsx'));
     return true;
   } catch (error) {
     dialog.showMessageBox({
       title: 'Error',
-      message: `Arquivo ENVELOPE.xlsx não localizado, \n verifique em ${app.getPath(
-        'userData'
-      )} `,
+      message: `Arquivo ${join(
+        app.getPath('userData'),
+        '\\ENVELOPE.xlsx'
+      )} não localizado, \n verifique em ${app.getPath('userData')} `,
     });
     return false;
   }
